49d947b15bbbe42b8bb28bf308d1c55757bda26a
Matthew Bartl <mattbartl@gmail.com>
2019-04-05 20:51:37 +0000

    Automatically backed up by Learn

--- lib/book.rb
+++ lib/book.rb
@@ -7,6 +7,6 @@ class Book
   attr_accessor :author, :page_count, :genre
 
   def turn_page
-    puts "Flipping the page... wow, you read fast!"
+    puts "Flipping the page...wow, you read fast!"
   end
 end
